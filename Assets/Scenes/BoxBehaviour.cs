60568557b6215f2ed04582e934a1dcad47346d02
Tudor B. <tudors152005@gmail.com>
2023-12-09 20:21:14 +0200

    Ver. 0.02:  Solution found for C++ not being able to use strings: pass a char pointer and an int length as parameters to a function and on the C# side you use the same function but with StringBuilder and int length.
    Given the above overhead and other constricting factors, coding was switched to C# (at least for now).
    ---------------------------------------
    -added collision detection
    -added WASD handling (movement)
    -initial (broken) implementation for the jumping mechanics

--- Assets/Scenes/BoxBehaviour.cs
+++ Assets/Scenes/BoxBehaviour.cs
@@ -7,14 +7,20 @@ public class BoxBehaviour : MonoBehaviour
 {
 
     [DllImport("boxcoll")]
-
     private static extern int SimpleReturnFunc();
 
-    
+    public Rigidbody rb;
+    float distToGround = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(SimpleReturnFunc());
+
+        // get the distance to ground
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+        rb = GetComponent<Rigidbody>();
     }
 
     /*
@@ -24,4 +30,80 @@ public class BoxBehaviour : MonoBehaviour
         
     }
     */
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    bool isJumping = false;
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            Debug.Log("Detected key code: " + e.keyCode.ToString());
+            //Input.GetKeyDown(KeyCode.Space);
+
+            switch (e.keyCode.ToString())
+            {
+                case "W":
+                    transform.Translate(0, 0, 0.03f);
+                    break;
+                case "A":
+                    transform.Translate(-0.03f, 0, 0);
+                    break;
+                case "S":
+                    transform.Translate(0, 0, -0.03f);
+                    break;
+                case "D":
+                    transform.Translate(0.03f, 0, 0);
+                    break;
+                case "Space":
+                    if(!isJumping)
+                    {
+                        //transform.Translate(0, 0.03f, 0);
+                        rb.velocity = new Vector3(0, 10, 0);
+                        Debug.Log("JUUUUUUUUUUUUMP! ");
+                        isJumping = true;
+                    }
+                    break;
+                default:
+                    Debug.Log("Lala! ");
+                    break;
+            }
+        }
+
+        float tmpDist = GetComponent<Collider>().bounds.extents.y;
+
+        if (!isJumping && (tmpDist - distToGround >=0))
+        {
+            Debug.Log("Landed! ");
+            isJumping = false;
+        }
+
+            
+        Debug.Log("Dist to ground: " + tmpDist);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            print("Enter");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            print("Stay");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            print("Exit");
+        }
+    }
 }
